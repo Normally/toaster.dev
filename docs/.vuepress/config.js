8231903351a10a70920e0addb604a3314591138b
Nic Mulvaney <nic@nicmulvaney.com>
2022-01-20 15:54:05 +0000

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -9,6 +9,7 @@ module.exports = {
    * Ref：https://v1.vuepress.vuejs.org/config/#description
    */
   description: 'A free JSON placeholder API',
+  dest: 'docs/.vuepress/dist',
   /**
    * Extra tags to be injected to the page HTML `<head>`
    *
